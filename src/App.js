3907fe4c1d434f40f1fd396bfe6edfd24502fc1d
HifsaAnsari <hifsa.ansari12@gmail.com>
2021-08-23 19:52:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Ball from './component/Ball';
+import Lotty from './component/Lotty';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+{/* <Ball num={2}/>
+<Ball num={5}/>
+<Ball num={9}/>
+<Ball num={22}/> */}
+<Lotty/>
+<Lotty title='Mini Daily' numBall={4} maxNum={22}/>
+</>
   );
 }
 
